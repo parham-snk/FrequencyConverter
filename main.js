ee1bc77d71498b0e3ca89b5064e6c4221d8878c7
parham-snk <psanjanaki@gmail.com>
2026-01-18 22:07:58 +0330

    add progressBar(current-all) and perform async

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const { exec } = require("child_process")
+const { exec, execSync } = require("child_process")
 const { BrowserWindow, app, dialog, ipcMain } = require("electron")
 const path = require("path")
 let win;
@@ -27,7 +27,7 @@ app.whenReady().then(async () => {
         });
         if (!result.canceled) {
             inDir = result.filePaths[0]
-            win.webContents.send("updateNode",{id:"in_dir",value:inDir})
+            win.webContents.send("updateNode", { id: "in_dir", value: inDir })
 
         }
     })
@@ -39,7 +39,7 @@ app.whenReady().then(async () => {
 
         if (!out.canceled) {
             outdir = out.filePaths[0]
-            win.webContents.send("updateNode",{id:"out_dir",value:outdir})
+            win.webContents.send("updateNode", { id: "out_dir", value: outdir })
         }
     })
 
@@ -48,18 +48,46 @@ app.whenReady().then(async () => {
             if (err && stderr) {
                 return console.log(err)
             }
+
             const files = std.split(/\r\n/).filter(item => {
-                if (item.split(".").length > 1 && item.split(".")[1] == "mp3") {
+                if (item.split(".").length > 1 && item.split(".")[(item.split(".").length) - 1] == "mp3") {
                     return item
                 }
             }).map(item => item.trim())
-            files.forEach(file => {
-                const dir = path.join(inDir, file)
-                const output = path.join(outdir, file)
-                exec(`ffmpeg -i "${dir}" -af \"asetrate=44100*432/440\"  "${output}"`, (err, std, stderr) => {
-                    win.webContents.send("refresh", { name: file, dir: output })
-                })
-            })
+            win.webContents.send("updateNode", { id: "qty", value: `( ${files.length} items)` })
+            win.webContents.send("updateNode", { id: "total", value: files.length })
+            // files.forEach(async (file, index) => {
+            // const dir = path.join(inDir, file)
+            // const output = path.join(outdir, file)
+            // await execSync(`ffmpeg -i "${dir}" -af \"asetrate=44100*432/440\"  "${output}"`, (err, std, stderr) => {
+            //     if (err || stderr) {
+            //         // console.log(`err : ${err}`)
+            //         // console.log(`stderr : ${stderr}`)
+            //     }
+            //     win.webContents.send("updateNode", { id: "passed", value: index })
+
+            //     win.webContents.send("refresh", { name: file, dir: output })
+
+            // })
+            // })
+            function generate(index) {
+                if (index != files.length) {
+                    const dir = path.join(inDir, files[index])
+                    const output = path.join(outdir, files[index])
+                    exec(`ffmpeg -i "${dir}" -af \"asetrate=44100*432/440\"  "${output}"`, (err, std, stderr) => {
+                        if (err || stderr) {
+                            // console.log(`err : ${err}`)
+                            // console.log(`stderr : ${stderr}`)
+                        }
+                        win.webContents.send("updateNode", { id: "passed", value: (index+1) })
+
+                        win.webContents.send("refresh", { name: files[index], dir: output })
+                        index+=1
+                        generate(index)
+                    })
+                }
+            }
+            generate(0)
         })
     })
 })
